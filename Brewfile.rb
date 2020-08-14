1856582023407a20ce69ed7ff87925bc5df2e9ab
Peter Hauck <peter@pggb.net>
2020-08-14 00:42:08 +0000

    Initial Update

---
+++ Brewfile.rb
@@ -0,0 +1,36 @@
+cask_args appdir: "/Applications"
+tap "homebrew/cask-fonts"
+
+brew "mas"
+
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+tap "homebrew/core"
+brew "fish"
+brew "python"
+brew "pipenv"
+brew "wimlib"
+brew "node"
+cask "alfred"
+cask "pycharm"
+cask "tower"
+cask "vmware-fusion"
+cask "docker"
+cask "dropbox"
+mas "BBEdit", id: 404009241
+mas "Brother iPrint&Scan", id: 1193539993
+mas "Magnet", id: 441258766
+mas "Microsoft Excel", id: 462058435
+mas "Microsoft OneNote", id: 784801555
+mas "Microsoft Outlook", id: 985367838
+mas "Microsoft PowerPoint", id: 462062816
+mas "Microsoft Remote Desktop", id: 1295203466
+mas "Microsoft Word", id: 462054704
+mas "OneDrive", id: 823766827
+mas "Pages", id: 409201541
+mas "Reeder", id: 1449412482
+mas "SnippetsLab", id: 1006087419
+mas "Spark", id: 1176895641
+mas "SubnetCalc", id: 412946682
+mas "Xcode", id: 497799835
---
+++ install.command
@@ -0,0 +1,3 @@
+cp Brewfile.rb ~/Brewfile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew bundle install
