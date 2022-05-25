dfa4aa6633fce7f8f9436ef16d3e4ea311d265a7
Peter Hauck <peter@pggb.net>
2022-05-25 18:50:25 +1000

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,3 @@
-cd ~/dev/git
-git clone https://github.com/rainpole/vmware-stencils.git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/peteha/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
